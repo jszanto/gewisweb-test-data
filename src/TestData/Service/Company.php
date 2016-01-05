9af6f445902fe38254e532df4c877535cee30ab7
Justin Szanto <jszanto@users.noreply.github.com>
2016-01-06 00:27:07 +0100

    Fix multiple bugs in company and job generation

--- src/TestData/Service/Company.php
+++ src/TestData/Service/Company.php
@@ -16,8 +16,8 @@ class Company extends AbstractTestDataService
     public function generateCompany()
     {
         $company = new CompanyModel();
-        $companyDutch = new CompanyI18nModel();
-        $companyEnglish = new CompanyI18nModel();
+        $companyDutch = new CompanyI18nModel('nl', $company);
+        $companyEnglish = new CompanyI18nModel('en', $company);
 
         $name = $this->faker->word;
         $company->setName($name);
@@ -25,29 +25,26 @@ class Company extends AbstractTestDataService
         $company->setAddress($this->faker->address);
         $company->setEmail($this->faker->companyEmail);
         $company->setPhone($this->faker->phoneNumber);
+        $company->setHidden(false);
         $path = $this->faker->image('/tmp', 200, 40);
-        $logo = $this->getFileStorageService()->storeUploadedFile($path);
+        $logo = $this->getFileStorageService()->storeFile($path);
         $companyDutch->setWebsite($this->faker->url);
         $companyDutch->setSlogan($this->faker->sentence);
         $companyDutch->setLogo($logo);
         $companyDutch->setDescription($this->faker->text());
-        $companyDutch->setLanguage('nl');
 
         $companyEnglish->setWebsite($this->faker->url);
         $companyEnglish->setSlogan($this->faker->sentence);
         $companyEnglish->setLogo($logo);
         $companyEnglish->setDescription($this->faker->text());
-        $companyEnglish->setLanguage('nl');
-        $this->em->persist($company);
-        $company->addTranslation($companyDutch);
-        $company->addTranslation($companyEnglish);
+
         $this->em->persist($company);
         $package = new JobPackageModel();
-        $package->setCompan($company);
+        $package->setCompany($company);
         $startDate = $this->faker->dateTimeBetween('-1 years','now');
         $endDate = $this->faker->dateTimeBetween($startDate, '+1 years');
-        $package->setStartingDate(new \DateTime($startDate));
-        $package->setExpirationDate(new \DateTime($endDate));
+        $package->setStartingDate($startDate);
+        $package->setExpirationDate($endDate);
         $package->setPublished(true);
         $this->em->persist($package);
         for ($i = 0; $i < $this->faker->randomDigit; $i++) {
@@ -69,7 +66,17 @@ class Company extends AbstractTestDataService
         $job->setEmail($this->faker->companyEmail);
         $job->setDescription($this->faker->text());
 
+        $job->setPackage($package);
         $this->em->persist($job);
-        $package->addJob($job);
+    }
+
+    /**
+     * Gets the storage service.
+     *
+     * @return \Application\Service\Storage
+     */
+    public function getFileStorageService()
+    {
+        return $this->sm->get('application_service_storage');
     }
 }
