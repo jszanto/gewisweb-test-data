853fd41339b04e77bd8bb14fb88d9665093cb6ee
Justin Szanto <jszanto@users.noreply.github.com>
2016-01-05 23:41:56 +0100

    Fix a few bugs in photo generation

--- src/TestData/Service/Photo.php
+++ src/TestData/Service/Photo.php
@@ -19,7 +19,7 @@ class Photo extends AbstractTestDataService
             $endDateTime = $this->faker->dateTime->format("Y-m-d H:i:s");
             $startDateTime = $this->faker->dateTime($endDateTime)->format("Y-m-d H:i:s");
             $albumCount = (rand(0, 1) == 1) ? rand(1, 3) : 0;
-            $photoCount = $this->faker->numberBetween(0, 250);
+            $photoCount = $this->faker->numberBetween(0, 100);
             $name = join($this->faker->words(2), ' ');
             $album = new AlbumModel();
             $album->setName($name);
@@ -29,14 +29,14 @@ class Photo extends AbstractTestDataService
                 $album->setParent($parent);
 
             }
-
+            $this->em->persist($album);
             for ($i = 0; $i < $photoCount; $i++) {
-                $this->generatePhotos($album);
+                $this->generatePhoto($album);
             }
             for ($i = 0; $i < $albumCount; $i++) {
                 $this->generateAlbum($reclevel + 1, $album);
             }
-            $this->em->persist($album);
+
             $this->em->flush();
 
         }
@@ -52,9 +52,19 @@ class Photo extends AbstractTestDataService
         $photo->setFlash($this->faker->randomElement(array(0, 1)));
         $photo->setFocalLength($this->faker->numberBetween(10, 200));
         $photo->setExposureTime($this->faker->randomFloat(3, 0, 10));
-        $photo->setShutterSpeed($this->faker->numerify('1/#00');
-        $photo->setAperture('f' . $this->faker->randomFloat(1, 2, 10);
+        $photo->setShutterSpeed($this->faker->numerify('1/#00'));
+        $photo->setAperture('f' . $this->faker->randomFloat(1, 2, 10));
         $photo->setIso($this->faker->numerify('#000'));
         $this->em->flush();
+    }
+
+    /**
+     * Gets the photo admin service.
+     *
+     * @return \Photo\Service\Admin
+     */
+    public function getAdminService()
+    {
+        return $this->sm->get('photo_service_admin');
     }
 }
